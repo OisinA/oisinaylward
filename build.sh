d6325116f573db5ef26f20483b1818e3ca0293f8
OisinA <oisinaylward@gmail.com>
2022-02-10 00:05:53 +0000

    Specify version

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+wget -qO- https://github.com/thedodd/trunk/releases/download/v0.14.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 trunk build
