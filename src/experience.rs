0d5a16a6e7813921c202e106bdd57a55dd2c6db6
OisinA <oisinaylward@gmail.com>
2022-02-10 20:04:34 +0000

    Tidy up a bit

--- src/experience.rs
+++ src/experience.rs
@@ -10,6 +10,35 @@ pub struct Experience<'a> {
     pub end_date: Option<Date<Utc>>,
 }
 
+impl<'a> Experience<'a> {
+    fn get_duration_str(&self) -> String {
+        let end_date = self.end_date.unwrap_or_else(|| Utc::now().date());
+        let duration = end_date - self.start_date;
+        let mut duration_str = String::new();
+        if duration.num_days() >= 364 {
+            let years = duration.num_days() / 364;
+            duration_str += &format!("{} year{}", years, if years > 1 { "s" } else { "" });
+            let months = (duration.num_days() % 364) / 30;
+            if months != 0 {
+                duration_str += &format!(
+                    " {} month{}",
+                    (duration.num_days() % 364) / 30,
+                    if months > 1 { "s" } else { "" }
+                );
+            }
+        } else {
+            let months = (duration.num_days() % 364) / 30;
+            duration_str += &format!(
+                "{} month{}",
+                duration.num_days() / 30,
+                if months > 1 { "s" } else { "" }
+            );
+        }
+
+        duration_str
+    }
+}
+
 #[derive(Properties, PartialEq)]
 pub struct ExperienceProps<'a> {
     pub experiences: Vec<Experience<'a>>,
@@ -61,43 +90,17 @@ pub fn experience_list(ExperienceProps { experiences }: &ExperienceProps<'static
 
 #[function_component(ExperienceItem)]
 pub fn experience_item(ExperienceItemProps { experience }: &ExperienceItemProps<'static>) -> Html {
-    let end_date = if let Some(date) = experience.end_date {
-        date
-    } else {
-        Utc::now().date()
-    };
-
-    let duration = end_date - experience.start_date;
-    let mut duration_str = String::new();
-    if duration.num_days() >= 364 {
-        let years = duration.num_days() / 364;
-        duration_str += &format!("{} year{}", years, if years > 1 { "s" } else { "" });
-        let months = (duration.num_days() % 364) / 30;
-        if months != 0 {
-            duration_str += &format!(
-                " {} month{}",
-                (duration.num_days() % 364) / 30,
-                if months > 1 { "s" } else { "" }
-            );
-        }
-    } else {
-        let months = (duration.num_days() % 364) / 30;
-        duration_str += &format!(
-            "{} month{}",
-            duration.num_days() / 30,
-            if months > 1 { "s" } else { "" }
-        );
-    }
+    let duration_str = experience.get_duration_str();
 
     html! {
-    <li class="mb-5">
-        <h2 class="text-2xl bold">{ experience.title } { " @ " } <a class="text-blue-600" href={ experience.company_link }>{ experience.company }</a></h2>
-        <p>{ experience.start_date.format("%B %Y") } { " - " } { if experience.end_date.is_none() {
-            "Present".to_string()
-        } else {
-            format!("{}", experience.end_date.unwrap().format("%B %Y"))
-        }}</p>
-        <p class="text-xs text-slate-500"> { duration_str } </p>
-    </li>
+        <li class="mb-5">
+            <h2 class="text-2xl bold">{ experience.title } { " @ " } <a class="text-blue-600" href={ experience.company_link }>{ experience.company }</a></h2>
+            <p>{ experience.start_date.format("%B %Y") } { " - " } { if experience.end_date.is_none() {
+                "Present".to_string()
+            } else {
+                format!("{}", experience.end_date.unwrap().format("%B %Y"))
+            }}</p>
+            <p class="text-xs text-slate-500"> { duration_str } </p>
+        </li>
     }
 }
