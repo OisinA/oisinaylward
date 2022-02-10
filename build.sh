77e7b0afe79952d27126777eb65af841e0d00d2f
OisinA <oisinaylward@gmail.com>
2022-02-10 00:19:32 +0000

    Build with release flag

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ source ~/.cargo/env
 rustup target add wasm32-unknown-unknown
 
 wget -qO- https://github.com/thedodd/trunk/releases/download/v0.14.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
-./trunk build
+./trunk build --release
