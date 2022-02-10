bab644a691f99b895b9cc8ca8ee7d76ff4153a5f
OisinA <oisinaylward@gmail.com>
2022-02-10 00:08:15 +0000

    Specify version

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
+source ~/.cargo/env
+rustup target add wasm32-unknown-unknown
+
 wget -qO- https://github.com/thedodd/trunk/releases/download/v0.14.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 ./trunk build
