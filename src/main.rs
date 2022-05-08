746b3f1b7ee5f3effeabf9ccd6d5bb357a7f70fe
OisinA <oisinaylward@gmail.com>
2022-05-08 16:28:35 +0100

    Update Workday title

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ mod experience;
 lazy_static! {
     static ref EXPERIENCE: Vec<Experience<'static>> = vec![
         Experience {
-            title: "Software Development Engineer in DevOps",
+            title: "Software Development Engineer I",
             company: "Workday",
             company_link: "https://www.workday.com/",
             start_date: Utc.ymd(2021, 9, 27),
